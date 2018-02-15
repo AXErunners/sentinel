e34c555f6e4a570a1ef6d16b39e9d10e24ea77d1
-k <slowdive@me.com>
2018-02-14 20:04:43 -0500

    update

--- test/unit/test_axe_config.py
+++ test/unit/test_axe_config.py
@@ -59,7 +59,7 @@ def test_get_rpc_creds():
         assert key in creds
     assert creds.get('user') == 'axerpc'
     assert creds.get('password') == 'EwJeV3fZTyTVozdECF627BkBMnNDwQaVLakG3A4wXYyk'
-    assert creds.get('port') == 19998
+    assert creds.get('port') == 9936
 
 
 # ensure axe network (mainnet, testnet) matches that specified in config
