65134a8b208149f5a678b9b788230439b55f081b
-k <slowdive@me.com>
2018-11-18 22:18:34 -0500

    edit tc

--- test/unit/test_axe_config.py
+++ test/unit/test_axe_config.py
@@ -59,7 +59,7 @@ def test_get_rpc_creds():
         assert key in creds
     assert creds.get('user') == 'axerpc'
     assert creds.get('password') == 'EwJeV3fZTyTVozdECF627BkBMnNDwQaVLakG3A4wXYyk'
-    assert creds.get('port') == 19998
+    assert creds.get('port') == 19337
 
 
 def test_slurp_config_file():
